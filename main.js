c179f13c8759fdbdfc5cf339eb4331ad4c0872ba
Matt Silva <mmunizsi01@gmail.com>
2019-06-14 16:31:23 -0400

    updated js

--- main.js
+++ main.js
@@ -1,23 +1,58 @@
-const x = document.querySelectorAll(".select-style");
-const y = document.querySelectorAll("select");
+const x = document.querySelectorAll('.select-style');
+const y = document.querySelectorAll('select');
 class Select {
   // Create a new element item
-  constructor(_value) {
+  constructor(_value, _element, _className) {
     this.a;
+    this.b;
+    this.c;
+    this.i = 0;
     this.value = _value;
+    this.element = _element;
+    this.className = _className;
   }
-  createNewElement(element, className, text) {
-    this.a = document.createElement(element);
-    this.a.setAttribute("class", className);
+  createNewElement(text) {
+    this.a = document.createElement(this.element);
+    this.a.setAttribute('class', this.className);
     this.a.innerHTML = text;
   }
-  doSomething() {
-    for (let i = 0; i < this.value.length; i++) {
-      this.createNewElement("div", "select-selected", y[i].innerHTML);
-      this.value[i].appendChild(this.a);
+  doSomething(e) {
+    for (this.i; this.i < this.value.length; this.i++) {
+      this.b = e[this.i].options[0].innerHTML;
+      // this.c = e[this.i].innerHTML;
+      this.createNewElement(this.b);
+      this.value[this.i].appendChild(this.a);
+
+      this.a.addEventListener('click', e => {
+        console.log(e.target.nextSibling);
+        e.target.nextSibling.classList.toggle('select-hide');
+        e.target.classList.toggle('select-arrow-active');
+      });
+    }
+  }
+  doSomething2(e) {
+    for (this.i; this.i < this.value.length; this.i++) {
+      this.c = e[this.i].innerHTML;
+      console.log(this.c);
+      this.createNewElement(this.c);
+      this.value[this.i].appendChild(this.a);
+
+      this.a.addEventListener('click', e => {
+        const s = e.target.parentNode.parentNode.querySelectorAll('select')[0];
+        const h = e.target.parentNode.previousSibling;
+        h.innerHTML = e.target.innerHTML;
+
+        if (h.innerHTML == e.target.innerHTML) {
+          e.target.setAttribute('class', 'same-as-selected');
+        }
+
+        h.click();
+      });
     }
   }
 }
 
-const createDiv = new Select(x);
-createDiv.doSomething();
+const a = new Select(x, 'div', 'select-selected');
+a.doSomething(y);
+const b = new Select(x, 'div', 'select-items select-hide');
+b.doSomething2(y);
