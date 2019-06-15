78b6ac53eaf49104e0dd05ba8c183924a7632d8c
matt <mmunizsi01@gmail.com>
2019-06-14 20:22:37 -0400

    updated js

--- main.js
+++ main.js
@@ -16,19 +16,21 @@ class Select {
     this.a.setAttribute("class", this.className);
     this.a.innerHTML = text;
   }
-  doSomething(e) {
+  addElements(e) {
     for (this.i; this.i < this.value.length; this.i++) {
       this.b = e[this.i].options[0].innerHTML;
       this.createNewElement(this.b);
       this.value[this.i].appendChild(this.a);
 
       this.a.addEventListener("click", e => {
+        e.stopPropagation();
         e.target.nextSibling.classList.toggle("select-hide");
         e.target.classList.toggle("select-arrow-active");
+        this.closeAllSelect(e.target);
       });
     }
   }
-  doSomething2(e) {
+  addElements2(e) {
     for (this.i; this.i < this.value.length; this.i++) {
       this.c = e[this.i].innerHTML;
       this.createNewElement(this.c);
@@ -45,10 +47,30 @@ class Select {
       });
     }
   }
-  closeAllSelect(className) {}
+  closeAllSelect(element) {
+    var x_,
+      y_,
+      i,
+      arrNo = [];
+    x_ = document.querySelectorAll(".select-items");
+    y_ = document.querySelectorAll(".select-selected");
+    for (i = 0; i < y_.length; i++) {
+      if (element == y_[i]) {
+        arrNo.push(i);
+      } else {
+        y_[i].classList.remove("select-arrow-active");
+      }
+    }
+    for (i = 0; i < x_.length; i++) {
+      if (arrNo.indexOf(i)) {
+        x_[i].classList.add("select-hide");
+      }
+    }
+  }
 }
 
 const a = new Select(x, "div", "select-selected");
-a.doSomething(y);
+a.addElements(y);
 const b = new Select(x, "div", "select-items select-hide");
-b.doSomething2(y);
+b.addElements2(y);
+document.addEventListener("click", a.closeAllSelect);
